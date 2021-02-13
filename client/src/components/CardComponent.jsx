4c2992ce29b19ed6eadd0fbc5ca16b10f634e9d2
Ediyasaedi <ediyasa2724@gmail.com>
2021-02-13 21:54:19 +0700

    add final

--- client/src/components/CardComponent.jsx
+++ client/src/components/CardComponent.jsx
@@ -3,35 +3,25 @@ import React from "react"
 
 function CardComponent(props) {
     return (
-        <Box maxW="sm" borderWidth="1px" rounded="lg" overflow="hidden">
-            <Box p="6">
-                <Box d="flex" alignItems="baseline">
-                    <Badge rounded="full" px="2" variantColor="teal">
-                        Status
-                    </Badge>
-                    <Box
-                        color="gray.500"
-                        fontWeight="semibold"
-                        letterSpacing="wide"
-                        fontSize="xs"
-                        textTransform="uppercase"
-                        ml="2"
-                    >
-                        {props.status}
-                    </Box>
-                </Box>
-
-                <Box
-                    mt="1"
-                    fontWeight="semibold"
-                    as="h4"
-                    lineHeight="tight"
-                    isTruncated
-                >
-                    {props.title}
-                </Box>
-            </Box>
-        </Box>
+        <Stack isInline>
+            <Image
+                size="100px"
+                objectFit="cover"
+                src="https://bit.ly/sage-adebayo"
+                alt="Segun Adebayo"
+            />
+            <Image
+                size="150px"
+                objectFit="cover"
+                src="https://bit.ly/dan-abramov"
+                alt="Dan Abramov"
+            />
+            <Image
+                size="200px"
+                src="https://bit.ly/dan-abramov"
+                alt="Dan Abramov"
+            />
+        </Stack>
     )
 }
 
